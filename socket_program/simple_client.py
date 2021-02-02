fd841a093d457e26f089225bf7c6edeeea1321da
hugolin615 <hugolin615@gmail.com>
2021-02-01 23:29:49 -0500

    adding some print messages

--- socket_program/simple_client.py
+++ socket_program/simple_client.py
@@ -9,11 +9,15 @@ PORT = 65432        # The port used by the server
 def main():
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        input('Prepare to make connection. (Press ENTER to execute s.connect() )')
         s.connect((HOST, PORT))
+        input('Prepare to send data. (Press ENTER to execute s.sendall() )')
         s.sendall(b'Hello, world')
         data = s.recv(1024)
 
-    print('Received', repr(data))
+        print('Received', repr(data))
+        input('Prepare to close the connection.(Press ENTER to execute s.close() )')
+        s.close()
 
 if __name__ == "__main__":
     main()
--- socket_program/simple_server.py
+++ socket_program/simple_server.py
@@ -8,15 +8,22 @@ PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 def main():
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
+        s.bind((HOST, PORT)) 
         s.listen()
+        print('Waiting for client to connect.')
+
         conn, addr = s.accept()
+
         with conn:
             print('Connected by', addr)
             while True:
                 data = conn.recv(1024)
+                print('Waiting for data to receive.')
                 if not data:
+                    print('An packet without payload is received, end the connection.')
                     break
+                else:
+                    print('Receiving data from a client.')
                 conn.sendall(data)
 
 if __name__ == "__main__":
