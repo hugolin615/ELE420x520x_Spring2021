3508285a3450166887feeb65d63a3cf38d9ef53c
hugolin615 <hugolin615@gmail.com>
2021-04-14 19:52:31 -0400

    update some scripts in zeek_tutorial

--- zeek_tutorial/run_globalvariable.sh
+++ zeek_tutorial/run_globalvariable.sh
@@ -1,3 +1,3 @@
 sudo rm *.log
 
-zeek -Cr data_aggregator.pcap ./dnp3m_analyzer/dnp3m.hlto ./global_variable.zeek
+zeek -Cr data_aggregator.pcap ./dnp3m_analyzer/dnp3m-analyzer.hlto ./global_variable.zeek
